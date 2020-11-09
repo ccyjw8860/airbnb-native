49a18a921d0643656c5f7b78c3dd6d17462a5676
ccyjw8860 <ccyjw8860@gmail.com>
2020-11-09 21:51:21 +0900

    #8.2 Welcom Screen Part One

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
-import Welcome from '../screens/Welcome';
-import SignUp from '../screens/SignUp';
-import SignIn from '../screens/SignIn';
+import React from "react";
+import { Platform, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import Welcome from "../screens/Welcome";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+import BackBtn from "../components/Auth/BackBtn";
 
 const Auth = createStackNavigator();
 
-export default () => <Auth.Navigator>
-    <Auth.Screen name='Welcome' component={Welcome} />
-    <Auth.Screen name='SignIn' component={SignIn} />
-    <Auth.Screen name='SignUp' component={SignUp} />
+const isAndroid = Platform.OS === "android";
+
+export default () =>  (
+    <Auth.Navigator
+        mode="modal"
+        screenOptions={{
+        headerBackTitleVisible: false,
+        headerTransparent: true,
+        headerBackImage: () => <BackBtn/>
+        }}
+        >
+            <Auth.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{
+                    headerTitleStyle: {
+                    color: "white"
+                    }
+                }}
+                />
+            <Auth.Screen name='SignIn' component={SignIn} />
+            <Auth.Screen name='SignUp' component={SignUp} />
 </Auth.Navigator>
+) 
